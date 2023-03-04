22ac4ff06f52185b8a19de3ca39a20f3396652a7
patrickojiambo <pachrick237@students.uonbi.ac.ke>
2023-03-04 10:41:58 +0000

    Removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 
-
 void main() {
-  runApp(const HomePage());
+  runApp(const MyApp());
 }
 
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'SpendSense',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
